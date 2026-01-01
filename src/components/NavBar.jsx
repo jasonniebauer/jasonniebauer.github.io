a28ccc1cdd185239b22106bd0835a04dd95d5549
jasonniebauer <lookwhatjasondid@gmail.com>
2025-12-31 20:06:10 -0600

    Add Blog to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,20 @@
 const NavBar = (props) => {
   const links = [
     {
+      title: 'Blog',
+      url: 'https://blog.jasonniebauer.com'
+    },
+    {
       title: 'About',
       url: '#about',
     },
     {
-      title: 'LinkedIn',
-      url: 'https://www.linkedin.com/in/jasonniebauer',
-    },
-    {
       title: 'GitHub',
       url: 'https://github.com/jasonniebauer'
+    },
+    {
+      title: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/jasonniebauer',
     },
   ];
 
